7c803ed7f4f8a59932113929a80954ac66622f63
Pedro de Oliveira <pedrooliveira2309@gmail.com>
2019-02-01 21:34:00 -0200

    Commit inicial

---
+++ src/main/java/br/com/ad/agendadodoutor/AgendaDoDoutorApplication.java
@@ -0,0 +1,14 @@
+package br.com.ad.agendadodoutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDoDoutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaDoDoutorApplication.class, args);
+    }
+
+}
+
